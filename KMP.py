2f8ffec0fd69b9f86ead1f5cf4a40e75c11b2c25
rheaanand96 <rheaanand96@gmail.com>
2016-10-06 17:40:01 +0530

    Create KMP.py

---
+++ KMP.py
@@ -0,0 +1,32 @@
+def RabinKarpMatcher (T,P,d,q):
+	n=len(T)
+	m=len(P)
+	h=(d**(m-1))%q
+	print(h)
+	p=0
+	t=0
+	for i in range(m):
+		p=(d*p+P[i])%q
+		t=(d*t+T[i])%q
+		print("p here ",p)
+		print("t here ",t)
+	for s in range(n-m+1):
+		print("t",t)
+		print("p",p)
+		print("s",s)
+		if p==t:
+			if P[:m]==T[s:s+m]:
+				print("pattern occurs with shift",s)
+		if s<(n-m):
+				t=(d*(t-T[s]*h)+T[s+m])%q
+				print("t now",t)
+data=[1,2,3,4,3,2,4]
+A=set(data)
+Alphabet=list(A)
+print(Alphabet)
+Pattern=[2,3]
+q=13
+d=10
+RabinKarpMatcher(data,Pattern,d,q)
+
+
