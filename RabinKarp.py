3a67774d85fa059c06a33364c3aa0458210364af
rheaanand96 <rheaanand96@gmail.com>
2016-10-13 10:51:58 +0530

    Update RabinKarp.py

--- RabinKarp.py
+++ RabinKarp.py
@@ -5,6 +5,7 @@ def RabinKarpMatcher (T,P,d,q):
 	print(h)
 	p=0
 	t=0
+	count=0
 	for i in range(m):
 		p=(d*p+P[i])%q
 		t=(d*t+T[i])%q
@@ -17,16 +18,17 @@ def RabinKarpMatcher (T,P,d,q):
 		if p==t:
 			if P[:m]==T[s:s+m]:
 				print("pattern occurs with shift",s)
+				count+=1
 		if s<(n-m):
 				t=(d*(t-T[s]*h)+T[s+m])%q
 				print("t now",t)
+	return count
 data=[1,2,3,4,3,2,4]
 A=set(data)
 Alphabet=list(A)
 print(Alphabet)
 Pattern=[2,3]
-q=13
-d=10
-RabinKarpMatcher(data,Pattern,d,q)
-
-
+def RKM(T,P):
+	q=13
+	d=10
+	return RabinKarpMatcher(T,P,d,q)
